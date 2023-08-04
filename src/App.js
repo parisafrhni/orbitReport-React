f43f72c2d6664f529e78e891467c8667ad0f6027
Parisafrhni94 <parisafarahanicsc@gmail.com>
2023-08-04 12:18:17 -0400

    p

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
 import Buttons from "./components/Buttons";
 import Table from "./components/Table";
-
+import Banner from "./components/Banner"; // Import the Banner component
+import { useState } from "react";
+import satData from "./components/satData";
 
 function App() {
+  const [sat, setSat] = useState(satData);
+  const displaySats = [...new Set(satData.map((data) => data.orbitType))];
+  const filterByType = (currentType) => {
+    if (currentType === 'All') {
+      setSat(satData);
+    } else {
+      const displaySats = satData.filter((newSatDisplay) => {
+        return newSatDisplay.orbitType === currentType;
+      });
+      setSat(displaySats);
+    }
+  };
   return (
     <div>
-      <Buttons />
-      <Table />
+      <Buttons
+        filterByType={filterByType}
+        setSat={setSat}
+        displaySats={displaySats}
+      />
+      <Table sat={sat} />
+      <Banner />
     </div>
   );
 }
